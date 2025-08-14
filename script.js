c66a6050eaa23b4c7b49c59d340f3752a0ddadf4
Jake Sadowski <jake_sadowski@shilab.com>
2025-08-14 09:54:33 -0500

    Initial commit; Setup file structure; Portfolio layout

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('load', () => {
+    alert('Welcome to my Porfolio!');
+});
